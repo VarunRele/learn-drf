17402f96c5ded9cf75e2f21d2284e8947e97aa11
VarunRele <rele.varun1@gmail.com>
2024-12-31 19:36:24 +0530

    Routing done

--- frontend-app-leanr/src/app/app.routes.ts
+++ frontend-app-leanr/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
+    { path: 'signup', loadChildren: () => import('./components/signup/signup.component').then(m => m.SignupComponent) },
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: '**', component: NotFoundComponent}
 ];
